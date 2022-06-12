9ef388f46b7727987d162132416643eead9a4b47
ecaldera <e.calderac@hotmail.com>
2022-06-12 16:37:58 -0400

    create reducer, seccion 2-32 ok

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -18,10 +18,21 @@ contador: number;
 constructor( private store: Store<AppState>) {
   this.contador = 10;
 
-  this.store.subscribe( state => {
-    console.log(state);
-    this.contador = state.contador;
-  });
+  //Con esta configuracion escucha todos los estados del store
+  // this.store.subscribe( state => {
+  //   console.log(state);
+  //   this.contador = state.contador;
+  // });
+
+  //Con esta opcion escucha una opcion del store en especifico
+  // this.store.select('contador').subscribe( contador => {
+  //   console.log(contador);
+  //   //this.contador = state.contador;
+  // });
+
+  //misma opcion de arriba pero resumida
+  this.store.select('contador')
+      .subscribe(contador => this.contador = contador);
 
 }
 
--- 02-redux-app/src/app/contador/contador.reducer.ts
+++ 02-redux-app/src/app/contador/contador.reducer.ts
@@ -1,21 +1,32 @@
-import { Action } from "@ngrx/store";
+import { createReducer, on, Action } from "@ngrx/store";
 import { decrementar, incrementar } from "./contador.actions";
 
 
 
-export function contadorReducer( state: number = 10, action: Action){
+// export function contadorReducer( state: number = 10, action: Action){
 
-    switch( action.type ){
+//     switch( action.type ){
 
-        case incrementar.type:
-            return state + 1;
+//         case incrementar.type:
+//             return state + 1;
         
-        case decrementar.type:
-            return state -1;
+//         case decrementar.type:
+//             return state -1;
 
-        default:
-            return state;
+//         default:
+//             return state;
 
-    }
+//     }
 
-}
+// }
+
+export const initialState = 20;
+
+export const _contadorReducer = createReducer(initialState,
+  on(incrementar, (state) => state + 1),
+  on(decrementar, (state) => state - 1),
+);
+
+export function contadorReducer(state: number = initialState, action: Action) {
+    return _contadorReducer(state, action);
+  }
